69f019caaf5f44444022ddff7abc8e765205fe3e
Victoria <64774792+Viketk@users.noreply.github.com>
2020-09-08 17:13:04 +0300

    2.0

--- src/test/java/tests/PO_Homework.java
+++ src/test/java/tests/PO_Homework.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import test.java.pages.HomePage;
@@ -15,10 +16,13 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class PO_Homework {
     WebDriver driver;
     HomePage homePage;
+    WebDriverWait wait;
+
 
     @BeforeMethod
     public void setUp() {
@@ -28,26 +32,32 @@ public class PO_Homework {
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.MILLISECONDS);
-
+        wait  = new WebDriverWait(driver, 10 , 550 );
         homePage = new HomePage(driver);
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
     }
 
     @Test
-    public void test1() {
+    public void test1() throws InterruptedException {
         homePage.open();
         homePage.clickInput();
         WebElement inputButton = driver.findElement(By.xpath("//input[@class = 'search-form__input ng-untouched ng-pristine ng-valid']"));
         inputButton.sendKeys("iPhone");
         inputButton.sendKeys(Keys.ENTER);
+        //Thread.sleep(8000);
 
         List<WebElement> goods = driver.findElements(By.cssSelector("span.goods-tile__title"));
         for (WebElement good : goods) {
             String actualText = good.getText();
-            String expectedText = "iphone";
-            assertEquals(actualText.contains("iPhone"), expectedText);
-            System.out.println("qq");
+            assertTrue(actualText.contains("iPhone"));
+            //System.out.println("qq");
+
 
 
         }
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
